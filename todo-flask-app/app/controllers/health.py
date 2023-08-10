473667f0e42ba33a43bda584bc3d83ca957e4b77
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-10 18:02:45 +0200

    Make lint

--- todo-flask-app/app/controllers/health.py
+++ todo-flask-app/app/controllers/health.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Response
 
 health_blueprint = Blueprint("health_blueprint", __name__)
 
+
 @health_blueprint.get("/health_check")
 def health_check() -> Response:
     return "<p>Hello, World! ❤️</p>", 200
