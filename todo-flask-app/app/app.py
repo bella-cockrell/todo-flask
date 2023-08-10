c25b42c82a08b74c5b25d22759dd2cc7a6e866a4
mociarain <m.ociarain@gmail.com>
2023-08-10 15:34:27 +0200

    Add isort and run

--- todo-flask-app/app/app.py
+++ todo-flask-app/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, redirect, url_for
+from flask import Flask, Response, redirect, request, url_for
 
 posts = [
     {"id": 1, "description": "hello", "priority": 1},
