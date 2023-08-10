24601be65f10587d89b3c7a18a67c73f3155cf13
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-10 18:04:43 +0200

    Change db to mock response from ORM

--- todo-flask-app/app/db/db.py
+++ todo-flask-app/app/db/db.py
@@ -1,4 +1,11 @@
+from app.models.post import Post
+
+# posts = [
+#     {"id": 1, "description": "hello", "priority": 1},
+#     {"id": 2, "description": "hello again", "priority": 2},
+# ]
+
 posts = [
-    {"id": 1, "description": "hello", "priority": 1},
-    {"id": 2, "description": "hello again", "priority": 2},
+    Post(id=1, description="hello", priority=1),
+    Post(id=2, description="hello again", priority=2),
 ]
