4685ba6f10febaaaf6795599633f8e32d91a9045
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-11 10:57:49 +0200

    Add model unit test

--- todo-flask-app/tests/unit/models/test_post.py
+++ todo-flask-app/tests/unit/models/test_post.py
@@ -2,14 +2,14 @@ import pytest
 from app.models.post import Post
 
 
-def test_post_is_created():
-    new_post = Post(id=1, description="hello", priority=2)
-    assert type(new_post) == Post
-    assert new_post.id == 1
-    assert new_post.description == "hello"
-    assert new_post.priority == 2
+class TestModelPost:
+    def test_post_is_created(self):
+        new_post = Post(id=1, description="hello", priority=2)
+        assert type(new_post) == Post
+        assert new_post.id == 1
+        assert new_post.description == "hello"
+        assert new_post.priority == 2
 
-
-def test_post_is_not_created():
-    with pytest.raises(TypeError):
-        bad_post = Post(id=1)
+    def test_post_is_not_created(self):
+        with pytest.raises(TypeError):
+            bad_post = Post(id=1)
