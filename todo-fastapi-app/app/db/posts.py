8819de26fd3a671e140e9c6652cc2b45b90399a4
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-11 15:02:18 +0200

    Create mock db data

---
+++ todo-fastapi-app/app/db/posts.py
@@ -0,0 +1,5 @@
+posts = [
+    {"id": 1, "description": "hello", "priority": 1},
+    {"id": 2, "description": "hello again", "priority": 2},
+    {"id": 3, "description": "last minute prio", "priority": 1},
+]
