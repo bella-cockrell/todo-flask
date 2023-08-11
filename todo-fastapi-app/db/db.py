cb71955dfdeb7926e260a9f340eef83cfea09cdc
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-11 15:02:40 +0200

    Delete old mock data file

--- todo-fastapi-app/db/db.py
+++
@@ -1,5 +0,0 @@
-posts = [
-    {"id": 1, "description": "hello", "priority": 1},
-    {"id": 2, "description": "hello again", "priority": 2},
-    {"id": 3, "description": "last minute prio", "priority": 1},
-]
