9dec950d6dd178b1a3ecd40917bbde5da8d3c644
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-11 17:40:46 +0200

    Add datetime and validation to Post model

--- todo-fastapi-app/app/models/post_model.py
+++ todo-fastapi-app/app/models/post_model.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import datetime
 
 
 class Post(BaseModel):
     id: int
     description: str
     title: str | None = None
-    priority: int
+    priority: int = Field(gt=0, description="Priority must be greater than 0")
+    created_at: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
