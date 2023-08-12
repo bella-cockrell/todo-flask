c69218abe42c2817d6f19834a5a82b8116be354d
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-12 14:26:10 +0200

    Updated fake db with new name of PostModel

--- todo-fastapi-app/app/db/posts.py
+++ todo-fastapi-app/app/db/posts.py
@@ -1,9 +1,9 @@
-from app.models.post_model import Post
+from app.models.post_model import PostModel
 
 posts = [
-    Post(id=1, description="hello", priority=1, title="It's me"),
-    Post(id=2, description="hello again", priority=2, title="It's me 2"),
-    Post(id=3, description="last minute prio", priority=1),
+    PostModel(id=1, description="hello", priority=1, title="It's me"),
+    PostModel(id=2, description="hello again", priority=2, title="It's me 2"),
+    PostModel(id=3, description="last minute prio", priority=1),
 ]
 
 # posts = [
