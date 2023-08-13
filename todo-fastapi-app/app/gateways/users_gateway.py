ffa28af1aabe6e38c1c7fe0f443c28b92a4cde38
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-13 19:59:03 +0200

    Split users and posts gateways

--- todo-fastapi-app/app/gateways/users_gateway.py
+++ todo-fastapi-app/app/gateways/users_gateway.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
 
-from app.db import db_models
-from app.domain_models import post_domain_model, user_domain_model
-from app.domain_models.user_domain_model import UserInDBDomainModel
+from app.db.db_models import UserDbModel
+from app.domain_models.user_domain_model import UserInDBDomainModel, UserCreate
 from app.helpers.oauth2 import get_password_hash
 
 
@@ -13,50 +12,22 @@ def get_user(db, username: str | None) -> UserInDBDomainModel | None:
 
 
 # This is SqlAlchemy, not core Python
-def get_user_by_id(db: Session, user_id: int) -> db_models.UserDbModel | None:
-    return (
-        db.query(db_models.UserDbModel)
-        .filter(db_models.UserDbModel.id == user_id)
-        .first()
-    )
+def get_user_by_id(db: Session, user_id: int) -> UserDbModel | None:
+    return db.query(UserDbModel).filter(UserDbModel.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str) -> db_models.UserDbModel | None:
-    return (
-        db.query(db_models.UserDbModel)
-        .filter(db_models.UserDbModel.email == email)
-        .first()
-    )
+def get_user_by_email(db: Session, email: str) -> UserDbModel | None:
+    return db.query(UserDbModel).filter(UserDbModel.email == email).first()
 
 
-def get_all_users(
-    db: Session, skip: int = 0, limit: int = 100
-) -> list[db_models.UserDbModel]:
-    return db.query(db_models.UserDbModel).offset(skip).limit(limit).all()
+def get_all_users(db: Session, skip: int = 0, limit: int = 100) -> list[UserDbModel]:
+    return db.query(UserDbModel).offset(skip).limit(limit).all()
 
 
-def create_user(
-    db: Session, user: user_domain_model.UserCreate
-) -> db_models.UserDbModel:
+def create_user(db: Session, user: UserCreate) -> UserDbModel:
     hashed_password = get_password_hash(user.password)
-    db_user = db_models.UserDbModel(email=user.email, hashed_password=hashed_password)
+    db_user = UserDbModel(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
-
-
-def get_all_posts(
-    db: Session, skip: int = 0, limit: int = 100
-) -> list[db_models.PostDbModel]:
-    return db.query(db_models.PostDbModel).offset(skip).limit(limit).all()
-
-
-def create_post(
-    db: Session, post: post_domain_model.PostCreateDomainModel, user_id: int
-) -> db_models.PostDbModel:
-    db_post = db_models.PostDbModel(**post, owner_id=user_id)
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
