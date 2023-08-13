d307f53c943638b5da23a6ab39e8d534dc33e781
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-13 17:04:35 +0200

    Add db and models files

---
+++ todo-fastapi-app/app/db/db_models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+
+class UserDbModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    full_name = Column(String, index=True)
+    hashed_password = Column(String)
+
+    posts = relationship("PostDbModel", back_populates="owner")
+
+
+class PostDbModel(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    priority = Column(Integer, index=True)
+    created_at = Column(DateTime, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("UserDbModel", back_populates="posts")
