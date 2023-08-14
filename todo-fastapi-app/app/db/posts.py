756b11fa97d71600792c1dd29e7a78c1fe6d7dc2
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-14 12:05:49 +0200

    Rename

--- todo-fastapi-app/app/db/posts.py
+++ todo-fastapi-app/app/db/posts.py
@@ -1,11 +1,9 @@
-from app.domain_models.post_domain_model import PostDomainModel
+from app.schemas.post_schema import Post
 
 posts = [
-    PostDomainModel(id=1, description="hello", priority=1, title="It's me", owner_id=1),
-    PostDomainModel(
-        id=2, description="hello again", priority=2, title="It's me 2", owner_id=1
-    ),
-    PostDomainModel(id=3, description="last minute prio", priority=1, owner_id=1),
+    Post(id=1, description="hello", priority=1, title="It's me", owner_id=1),
+    Post(id=2, description="hello again", priority=2, title="It's me 2", owner_id=1),
+    Post(id=3, description="last minute prio", priority=1, owner_id=1),
 ]
 
 # posts = [
