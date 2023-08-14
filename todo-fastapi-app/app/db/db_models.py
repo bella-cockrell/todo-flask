4c3dcbff14f8c2ce05a039184987909197ea9160
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-08-14 12:05:42 +0200

    Add SqlAlchemy db models

--- todo-fastapi-app/app/db/db_models.py
+++ todo-fastapi-app/app/db/db_models.py
@@ -8,7 +8,7 @@ class UserDbModel(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
+    # username = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     full_name = Column(String, index=True)
     hashed_password = Column(String)
